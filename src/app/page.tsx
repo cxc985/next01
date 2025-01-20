94bcbbd61a60360c530be2daab6ac3c353d5b9ac
cxc <cxc985@163.com>
2025-01-20 16:52:48 +0800

    备案

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 // import Image from "next/image";
 
 export default function Home() {
-  return <div className="bg-sky-100 h-screen w-screen">home page</div>
+  return <div className="bg-sky-100 h-screen w-screen flex flex-col justify-between items-center">
+    <h1>home page11</h1>
+    <footer>
+      <div>
+      <a href="https://beian.miit.gov.cn/" target="_blank">您的备案号</a>
+      <a href="https://beian.miit.gov.cn/" target="_blank">浙ICP备2023011913号</a>
+      </div>
+    </footer>
+  </div>
 }
