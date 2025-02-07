ba970935b5e028bee2b76f46f0500268051a9505
cxc <cxc985@163.com>
2025-02-07 13:00:08 +0800

    备案1.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@
 export default function Home() {
   return <div className="bg-sky-100 h-screen w-screen flex flex-col justify-between items-center">
     <h1>home page11</h1>
-    <footer>
+    {/* <footer>
       <div>
       <a href="https://beian.miit.gov.cn/" target="_blank">浙ICP备2023011913号-1</a>
       </div>
-    </footer>
+    </footer> */}
   </div>
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+'use client'
+import React from 'react'
+export default function Footer() {
+  // const test = () => {
+  //   console.log(pathname);
+  // };
+  return (
+    <>
+      {' '}
+      <div>
+        <a href="https://beian.miit.gov.cn/" target="_blank">
+          浙ICP备2023011913号-1
+        </a>
+      </div>
+    </>
+  )
+}
